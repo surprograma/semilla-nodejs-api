7b35450014b82574ce41ee9e9a5d20d9b6bb4e59
Federico Aloi <federico.aloi@gmail.com>
2020-12-16 15:04:37 -0300

    Agregada base de datos de test

--- docker/postgres/init/crear-db.sh
+++ docker/postgres/init/crear-db.sh
@@ -2,7 +2,11 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER unahur_covid_dev WITH PASSWORD 'coronavirus';
+    CREATE USER unahur_covid WITH PASSWORD 'coronavirus';
+    
     CREATE DATABASE unahur_covid_dev;
-    GRANT ALL PRIVILEGES ON DATABASE unahur_covid_dev TO unahur_covid_dev;
+    GRANT ALL PRIVILEGES ON DATABASE unahur_covid_dev TO unahur_covid;
+    
+    CREATE DATABASE unahur_covid_test;
+    GRANT ALL PRIVILEGES ON DATABASE unahur_covid_test TO unahur_covid;
 EOSQL
