b7fb65ea3a57ac57d28a349bc89a4534e2cf4ba4
Federico Aloi <federico.aloi@gmail.com>
2020-12-16 22:41:30 -0300

    Arreglado test:watch

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   setupFilesAfterEnv: ['./lib/models/index.js', './jest.setup.js'],
   testEnvironment: 'node',
   testPathIgnorePatterns: ['./node_modules', './dist'],
+  modulePathIgnorePatterns: ['./docker'],
 };
