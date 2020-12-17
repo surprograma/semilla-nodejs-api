c291f97b9560cb33f8a5453403c94193e9efa652
Federico Aloi <federico.aloi@gmail.com>
2020-12-16 21:17:59 -0300

    Se guarda info de los seeders ejecutados

--- test/db_utils.js
+++ test/db_utils.js
@@ -6,9 +6,9 @@ declare
   l_stmt text;
 begin
   select 'truncate ' || string_agg(format('%I.%I', schemaname, tablename), ',') || ' RESTART IDENTITY CASCADE'
-    into l_stmt 
-  from pg_tables 
-  where schemaname in ('public') AND pg_tables.tablename not in ('SequelizeMeta', 'migrations'); 
+    into l_stmt
+  from pg_tables
+  where schemaname in ('public') AND pg_tables.tablename not in ('SequelizeMeta', 'SequelizeData');
   execute l_stmt;
 end;
 $$`;
